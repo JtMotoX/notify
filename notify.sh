0a59fa0be8c3ee73c3ed57e53f164ebdd2e8e9a7
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-12-01 02:48:11 -0800

    first commit

---
+++ notify.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# GET VARIABLES
+SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+ENV_FILE="${SCRIPTDIR}/.env"
+touch "${ENV_FILE}"
+if ! grep 'PUSHOVER_USER=' "${ENV_FILE}" >/dev/null 2>&1; then { echo "PUSHOVER_USER=" >>"${ENV_FILE}"; } fi
+if ! grep 'PUSHOVER_TOKEN=' "${ENV_FILE}" >/dev/null 2>&1; then { echo "PUSHOVER_TOKEN=" >>"${ENV_FILE}"; } fi
+. "${SCRIPTDIR}/.env"
+if [ "${PUSHOVER_USER}" = "" ]; then { echo "Please set the PUSHOVER_USER value: '${ENV_FILE}'"; FAILED=true; } fi
+if [ "${PUSHOVER_TOKEN}" = "" ]; then { echo "Please set the PUSHOVER_TOKEN value: '${ENV_FILE}'"; FAILED=true; } fi
+if [ "${FAILED}" = "true" ]; then { exit 1; } fi
+
+# GET COMMAND
+cmd="$@"
+
+# ESCAPE HASH CHARACTER
+cmd=${cmd//#/\\#}
+
+# DETERMINE OUTPUT FILE
+OUTFILE=/tmp/pushover_$(date +%s).txt
+
+# RUN COMMAND
+started=$(date +%s)
+bash -c "${cmd}" 2>&1 | tee "${OUTFILE}"
+EXITCODE=${PIPESTATUS[0]}
+ended=$(date +%s)
+runtime_s=$((ended-started))
+
+if [[ "${runtime_s}" -lt 60 ]]; then
+	runtime=${runtime_s}" sec"
+else
+	runtime=$((${runtime_s} / 60))" min"
+fi
+
+# SET TITLE
+if [[ "${EXITCODE}" == "0" ]]; then
+	TITLE="Success : ${runtime}"
+else
+	TITLE="Failed : ${runtime}"
+fi
+
+# GET LAST LINES FROM OUTPUT
+OUTLINES=$(cat "${OUTFILE}" | grep -v '^\s*$' | grep -v '^\x1b]' 2>/dev/null | tail -n 3)
+
+# SET MESSAGE
+MESSAGE="${OUTLINES}"
+
+# CLEANUP
+rm -f "${OUTFILE}"
+
+# SEND NOTIFICATION
+if [[ "${PUSHOVER}" == "0" ]] || [[ $(echo "${PUSHOVER}" | tr '[:upper:]' '[:lower:]') == "false" ]]; then
+	echo "Not sending push notification since PUSHOVER=${PUSHOVER}"
+else
+	curl -s \
+			--form-string "user=${PUSHOVER_USER}" \
+			--form-string "token=${PUSHOVER_TOKEN}" \
+			--form-string "title=${TITLE}" \
+			--form-string "message=${MESSAGE}" \
+			https://api.pushover.net/1/messages.json \
+			>/dev/null 2>&1
+fi
+
+# FUNCTION TO PLAY SOUND
+play_sound() {
+	SOUNDCOUND=$1
+	SOUNDSLEEP=$2
+	for i in `seq 1 ${SOUNDCOUND}`; do
+		printf "\a"
+		if [[ ${SOUNDCOUND} -ne ${i} ]]; then
+			sleep ${SOUNDSLEEP}
+		fi
+	done
+}
+
+# PLAY SOUND
+if [[ ${EXITCODE} -eq 0 ]]; then
+	play_sound 3 1
+else
+	play_sound 2 0.5
+	sleep 1
+	play_sound 2 0.5
+	sleep 1
+	play_sound 2 0.5
+	sleep 1
+fi
+
+exit ${EXITCODE}
